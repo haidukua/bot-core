dc164dbb4446e393af30ac0a8481386aa23657e5
haidukua <haidukua@gmail.com>
2025-06-26 00:19:00 +0300

    update bot

--- src/Bot.php
+++ src/Bot.php
@@ -9,7 +9,7 @@ final readonly class Bot
 {
     public function __construct(
         private AbstractRunner $runner,
-        private Scheduler $scheduler,
+        private Control $control,
     ) {
     }
 
@@ -18,15 +18,18 @@ final readonly class Bot
         try {
             $this->runner->onBoot($heroId);
             $this->runner->run($heroId);
+
             throw new Exception\SleepException();
         } catch (Exception\StopBotException $e) {
             $this->runner->onStopped($heroId, $e);
+            $this->control->stop($heroId);
 
             return;
         } catch (Exception\SleepException $e) {
             $sleepTime = $this->runner->sleepTime($heroId);
             $this->runner->onSleep($heroId, $sleepTime);
-            $this->scheduler->add($heroId, $sleepTime);
+
+            $this->control->start($heroId, $sleepTime);
 
             return;
         } finally {
